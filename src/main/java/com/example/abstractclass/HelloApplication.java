52e21261403aebf729b65ed9cd37041af2abe404
MrSport7 <belokopytov-andreyka@mail.ru>
2024-10-21 21:58:22 +0300

    Изменение заголовка приложения на 'Рисование фигур'

--- src/main/java/com/example/abstractclass/HelloApplication.java
+++ src/main/java/com/example/abstractclass/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
-        stage.setTitle("drawing figures");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
+        stage.setTitle("Рисование фигур");
         stage.setScene(scene);
         stage.show();
     }
